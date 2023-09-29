c5e723cd30e6da04533cba41b49b9093f45f2c54
Dani Martí <dmart126@boscdelacoma.cat>
2023-09-29 19:01:48 +0200

    Agregar archivos de proyecto.

---
+++ DAMWPFBinding/CodeBindingWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DAMWPFBinding
+{
+    /// <summary>
+    /// Lógica de interacción para CodeBindingWindow.xaml
+    /// </summary>
+    public partial class CodeBindingWindow : Window
+    {
+        public CodeBindingWindow()
+        {
+            InitializeComponent();
+
+            Binding binding = new Binding("Text");
+            binding.Source = this.tbValue;
+            this.tbNewValue.SetBinding(TextBlock.TextProperty, binding);
+        }
+    }
+}
